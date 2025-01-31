b142a613e3986101d071bb94f91d5faa704bdd20
Sean O'Mara <omarams@cua.edu>
2025-01-31 16:29:44 -0500

    tufte redux

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
-    darkModeToggle.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-    });
-});
